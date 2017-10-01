830782e6cd71fe997c908a57c66141caef1aa4ee
Laura Douglas <laura24douglas@gmail.com>
2017-10-01 16:51:38 +0100

    Adding firebase config and authentication to login page

--- app/src/main/java/com/hoophacks/hoophacks/Login.java
+++ app/src/main/java/com/hoophacks/hoophacks/Login.java
@@ -1,13 +1,170 @@
 package com.hoophacks.hoophacks;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private TextView tvStatus;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        tvStatus = (TextView) findViewById(R.id.tvStatus);
+
+        findViewById(R.id.bLogIn).setOnClickListener(this);
+        findViewById(R.id.bCreateUser).setOnClickListener(this);
+        findViewById(R.id.bLogOut).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d("Firebase", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d("Firebase", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateInfo(currentUser);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.bCreateUser) {
+            Log.d("Firebase", "create user");
+            createUser(etEmail.getText().toString(), etPassword.getText().toString());
+        } else if (i == R.id.bLogIn) {
+            Log.d("Firebase", "log in");
+            ///logIn(etEmail.getText().toString(), etPassword.getText().toString());
+        } else if (i == R.id.bLogOut) {
+            Log.d("Firebase", "logout");
+            ///logOut();
+        }
+    }
+
+    private void updateInfo(FirebaseUser user){
+        if (user != null) {
+            tvStatus.setText(getString(R.string.status_fmt, user.getEmail(), user.isEmailVerified()));
+            tvStatus.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            findViewById(R.id.email_password_buttons).setVisibility(View.GONE);
+            findViewById(R.id.email_password_fields).setVisibility(View.GONE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.VISIBLE);
+        } else {
+            tvStatus.setText("Logged Out");
+            tvStatus.setText(null);
+
+            findViewById(R.id.email_password_buttons).setVisibility(View.VISIBLE);
+            findViewById(R.id.email_password_fields).setVisibility(View.VISIBLE);
+            findViewById(R.id.signed_in_buttons).setVisibility(View.GONE);
+        }
+    }
+
+    private void createUser(String email, String password){
+        Log.d("Random", "createUser");
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("Random", "if");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateInfo(user);
+                        } else {
+                            Log.d("Random", "else");
+                            Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            updateInfo(null);
+                        }
+                    }
+                });
+        Log.d("Random", "createdUser");
+    }
+
+    private void logIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateInfo(user);
+                        } else {
+                            Toast.makeText(Login.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            updateInfo(null);
+                        }
+                    }
+                });
+    }
+
+    private void logOut(){
+        mAuth.signOut();
+        updateInfo(null);
+    }
+
+    private boolean validation(){
+        boolean valid = true;
+
+        String email = etEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            etEmail.setError("Required.");
+            valid = false;
+        } else {
+            etEmail.setError(null);
+        }
+
+        String password = etPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            etPassword.setError("Required.");
+            valid = false;
+        } else {
+            etPassword.setError(null);
+        }
+
+        return valid;
+    }
+
 }
